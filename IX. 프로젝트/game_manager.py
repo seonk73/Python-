f2113ab3be7dc826dd09a3b092c32a82a89cd09e
seonk73 <seonk73@naver.com>
2019-11-04 09:24:39 +0900

    TicTacToe GameManager_콘솔

--- IX. 프로젝트/game_manager.py
+++ IX. 프로젝트/game_manager.py
@@ -7,10 +7,22 @@ class GameManager:
     def play(self):
         # 게임 판 보여주기
         print(self.ttt)
-        # row, col 입력 받기
-        self.ttt.set(1, 1)
-        print(self.ttt)
-        # check_winnier이면 끝
+        while True:
+            # row, col 입력 받기
+            row = int(input("row : "))
+            col = int(input("col : "))
+            self.ttt.set(row, col)
+            print(self.ttt)
+            # check_winnier이면 끝
+            if  self.ttt.check_winner() == "O":
+                print("O win !")
+                break
+            elif self.ttt.check_winner() == "X":
+                print("X win !")
+                break
+            elif self.ttt.check_winner() == "d":
+                print("무승부")
+                break
 
 if __name__ == '__main__':
     gm = GameManager()
