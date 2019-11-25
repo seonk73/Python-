cf2a52b9b87f59bf968180e2820bdc4518586507
seonk73 <seonk73@naver.com>
2019-11-25 09:47:24 +0900

    Web Crawling - 연애혁명 제목, 링크 가져오기 -> html 파일 만들기

--- IX. 프로젝트/web_crawling.py
+++ IX. 프로젝트/web_crawling.py
@@ -9,10 +9,16 @@ if __name__ == '__main__':
     data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=570503")
     soup = BeautifulSoup(data, "lxml") # httpResponse -> HTML로 바꿔주기
     # print(soup)
+    html = "<html><head><meta charset='utf-8'></head><body>"
     cartoon_titles = soup.find_all("td", attrs={"class":"title"}) # <td class="title">...</td>
     for cartoon_title in cartoon_titles:
         title = cartoon_title.find("a").text # <a>text</a> & text를 하면 태그가 감싸져있는 제목만 가져올 수 있음
         link = cartoon_title.find("a").get("href") # <a href="">text</a> -> 태그의 속성값 가져오기
         link = "https://comic.naver.com" + link
-        print(title)
-        print(link)
+        # print(title)
+        # print(link)
+        html += "<a href='{}'>{}</a>".format(link, title) # <a href='link'>title</a>
+    html += "</body></html>"
+    # print(html)
+    with open("연애혁명.html", "w", encoding="utf-8") as f:
+        f.write(html) # html 파일 만들기
